a75c875c2f7b689cb0aa324990565b973f3be965
khaled-allak <kessam279@gmail.com>
2023-03-16 11:58:55 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include<unistd.h>
 /**
  * main - Entry point
  *
@@ -8,7 +8,7 @@
  * Return:1 (error)
  */
 int main(void)
-{	char quo[] = and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
