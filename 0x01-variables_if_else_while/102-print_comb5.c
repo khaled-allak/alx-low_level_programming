6f8db5f82e1830d262028d427618dc7a4f16057f
khaled-allak <kessam279@gmail.com>
2023-03-20 13:20:53 -0700

    a

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int firstdigit = 0, secondigit;
+
+	while (firstdigit <= 99)
+	{
+		secondigit = firstdigit;
+		while (secondigit <= 99)
+		{
+			if (secondigit != firstdigit)
+			{
+				putchar((firstdigit / 10) + 48);
+				putchar((firstdigit % 10) + 48);
+				putchar(' ');
+				putchar((secondigit / 10) + 48);
+				putchar((secondigit % 10) + 48);
+
+				if (firstdigit != 98 || secondigit != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondigit++;
+		}
+		firstdigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
