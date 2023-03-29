445b3664a7733a455823b0fd70b0e3c6f2d125ea
khaled-allak <kessam279@gmail.com>
2023-03-29 12:40:55 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,14 +15,22 @@
 */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
+	int c, i,
 
-	int index = 0, dest_len = 0;
+	    c = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
+	/*find size of dest array*/
+	while (dest[c])
+		c++;
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c + i] = src[i];
+	/*null terminate dest*/
+	dest[c + i] = '\0';
+
 	return (dest);
 }
