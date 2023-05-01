981120a25b15436b17e1aaa889ae51fcc2df367f
khaled-allak <kessam279@gmail.com>
2023-05-01 13:42:29 -0700

    a

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list of ints
+ * @head: pointer to the top of the list
+ *
+ * Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *node. * temp;
+
+if (!head)
+	return;
+
+node = *head;
+while (node)
+{
+	temp = node;
+	node = node->next;
+	free(temp);
+}
+
+*head = NULL;
+}
