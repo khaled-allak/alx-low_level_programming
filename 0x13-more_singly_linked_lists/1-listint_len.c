0bc6255d630ca102bd154778b51afacc4adc4575
khaled-allak <kessam279@gmail.com>
2023-05-01 11:35:53 -0700

    a

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+
+/**
+ *listint_len - print a number of linked lists
+ *@h: A pointer to the first of the list
+ *
+ *
+ *Return: the size of list
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
