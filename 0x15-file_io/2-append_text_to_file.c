3ab205aab039965c5fbc61345d2929b2f7a03d7c
khaled-allak <kessam279@gmail.com>
2023-05-12 04:14:02 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int _strlen(char *s)
  *
  * Return: 1 on success 0 on failure
 */
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
