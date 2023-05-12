690d04964340bfe71c96df41ca6f4d30781014ef
khaled-allak <kessam279@gmail.com>
2023-05-12 04:11:19 -0700

    a

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,13 +8,14 @@
 */
 int _strlen(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        if (!s)
-                return (0);
-        while (*s++)
-                i++;
-        return (i);
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
@@ -26,16 +27,16 @@ int _strlen(char *s)
 */
 int append_text_to_file(const char *filename, char *text_content);
 {
-        int fd;
-        ssize_t bytes = 0, len = _strlen(text_content);
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
-        if (!filename)
-                return (-1);
-        fd = open(filename, O_WRONLY | O_APPEND);
-        if (fd == -1)
-                return (-1);
-        if (len)
-                bytes = write(fd, text_content, len);
-        close(fd);
-        return (bytes == len ? 1 : -1);
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
+	return (bytes == len ? 1 : -1);
 }
