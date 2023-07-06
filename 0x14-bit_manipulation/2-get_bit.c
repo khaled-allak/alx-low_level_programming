75788c346cb332fe3e285d6705e6819e840b70af
khaled-allak <kessam279@gmail.com>
2023-07-06 07:08:53 -0700

    a

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,11 +11,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= sizeof(n) * 8)
 		return (-1);
-	if (index == 0)
-	{
-		return (n & 1);
-	}
-	return (get_bit(n >> 1, index - 1));
+
+	return (n >> index &1);
 }
