120c52ee09f4e4f34f74728c4361a32ee0cd7356
khaled-allak <kessam279@gmail.com>
2023-07-10 09:29:43 -0700

    a

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text from a file and prints it
+ * @filename: name of the readen file
+ * @letters: numbers of the readen bytes
+ *
+ * Return: number bytes read and printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &bud[0], bytes);
+	close(fd);
+	return (bytes);
+
+
+
+
